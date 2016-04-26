90cbea7d7081e1370eb3e431d3d8b56992ca612e
borzoni <borzoni@mail.ru>
2016-04-26 13:28:13 +0300

    res ~ s typo fixed

--- lib/sidekiq_crawler/card_parser.rb
+++ lib/sidekiq_crawler/card_parser.rb
@@ -59,7 +59,7 @@ module SidekiqCrawler
     
     def empty_selector?(s)
       return false if  s.is_a?(Numeric)
-      return (!res||res==""||res.empty)
+      return (!s||s==""||s.empty)
     end
     
     def normalize_str(str)
